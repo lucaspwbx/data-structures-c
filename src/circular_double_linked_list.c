c12f5ff4a8ad0e062df1429fdb479b53a7174511
Lucas Weiblen <lucasweiblen@gmail.com>
2016-10-21 06:30:41 -0200

    add header and draft of tests and implementation for circular double linked list

---
+++ src/circular_double_linked_list.c
@@ -0,0 +1,60 @@
+#include<stdlib.h>
+#include "circular_double_linked_list.h"
+
+struct node *create_list(int data, struct node *list) {
+  struct node *node;
+  node = (struct node *)malloc(sizeof(struct node));
+  node->data = data;
+  node->prev = node;
+  node->next = node;
+  list = node;
+  return list;
+}
+
+struct node *insert_end(int data, struct node *list) {
+  struct node *node, *ptr;
+  ptr = list;
+  node = (struct node *)malloc(sizeof(struct node));
+  node->data = data;
+  while (ptr->next != list)
+    ptr = ptr->next;
+  node->prev = ptr->prev;
+  node->next = ptr->next;
+  list->prev = node;
+  ptr->next = node;
+  return list;
+}
+
+struct node *insert_begin(int data, struct node *list) {
+  return list;
+}
+
+struct node *insert_after(int search_data, int data, struct node *list) {
+  return list;
+}
+
+struct node *insert_before(int search_data, int data, struct node *list) {
+  return list;
+}
+
+// adapt to circular double linked list
+struct node *delete_node(int search_data, struct node *list) {
+  struct node *ptr;
+  ptr = list;
+  if (ptr->data == search_data) {
+    ptr->next->next = ptr->next;
+    list = ptr->next;
+    return list;
+  }
+  while (ptr->next != list && ptr->next->data != search_data) {
+    ptr = ptr->next;
+  }
+  // searched node is the "last" node
+  if (ptr->next->next == list) {
+    ptr->next = list;
+  // searched node is in the middle of the list
+  } else {
+    ptr->next = ptr->next->next;
+  }
+  return list;
+}
---
+++ tests/circular_double_linked_list_test.c
@@ -0,0 +1,125 @@
+#include<stdarg.h>
+#include<stddef.h>
+#include<setjmp.h>
+#include<cmocka.h>
+#include<stdlib.h>
+#include "circular_double_linked_list.h"
+
+static void create_list_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  assert_int_equal(list->data, 5);
+  assert_non_null(list->prev);
+  assert_non_null(list->next);
+}
+
+static void insert_end_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  assert_int_equal(list->data, 5);
+  assert_int_equal(list->prev->data, 10);
+  assert_int_equal(list->next->data, 10);
+  assert_int_equal(list->next->prev->data, 5);
+  assert_int_equal(list->next->next->data, 5);
+}
+
+static void insert_begin_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_begin(10, list);
+  assert_int_equal(list->data, 10);
+  assert_int_equal(list->next->data, 5);
+  assert_int_equal(list->next->next->data, 5);
+}
+
+static void insert_after_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  list = insert_after(5, 7, list);
+  assert_int_equal(list->data, 5);
+  assert_int_equal(list->next->data, 7);
+  assert_int_equal(list->next->next->data, 10);
+  assert_int_equal(list->next->next->next->data, 5);
+}
+
+static void insert_before_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  list = insert_before(10, 7, list);
+  assert_int_equal(list->data, 5);
+  assert_int_equal(list->next->data, 7);
+  assert_int_equal(list->next->next->data, 10);
+  assert_int_equal(list->next->next->next->data, 5);
+}
+
+static void delete_node_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  list = delete_node(5, list);
+  assert_int_equal(list->data, 10);
+  assert_int_equal(list->next->data, 10);
+}
+
+static void delete_middle_node_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  list = insert_end(12, list);
+  list = delete_node(10, list);
+  assert_int_equal(list->data, 5);
+  assert_int_equal(list->next->data, 12);
+  assert_int_equal(list->next->next->data, 5);
+}
+
+static void delete_end_node_test(void **state) {
+  (void) state;
+
+  struct node *list;
+  list = (struct node *)malloc(sizeof(struct node));
+  list = create_list(5, list);
+  list = insert_end(10, list);
+  list = insert_end(12, list);
+  list = delete_node(12, list);
+  assert_int_equal(list->data, 5);
+  assert_int_equal(list->next->data, 10);
+  assert_int_equal(list->next->next->data, 5);
+}
+
+int main(void) {
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(create_list_test),
+    cmocka_unit_test(insert_end_test),
+   // cmocka_unit_test(insert_begin_test),
+   // cmocka_unit_test(insert_after_test),
+    //cmocka_unit_test(insert_before_test),
+   // cmocka_unit_test(delete_node_test),
+    //cmocka_unit_test(delete_middle_node_test),
+  //  cmocka_unit_test(delete_end_node_test),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
